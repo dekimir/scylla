48ff408d835c6ee3e764b360fa8c410780845f7a
Dejan Mircevski <dejan@scylladb.com>
2019-12-09 09:43:44 -0500

    Ensure enum_option blurb example compiles
    With the decision to return a value, map()s return type must change
    from const ref to value.
    
    Signed-off-by: Dejan Mircevski <dejan@scylladb.com>

--- utils/enum_option.hh
+++ utils/enum_option.hh
@@ -61,9 +61,9 @@ concept bool HasMapInterface = requires(T t) {
 ///
 /// struct Type {
 ///   enum ty { a1, a2, b1 };
-///   const static unordered_map<string, ty>& map();
+///   static unordered_map<string, ty> map();
 /// };
-/// const unordered_map<string, Type::ty>& Type::map() {
+/// unordered_map<string, Type::ty> Type::map() {
 ///   return {{"a1", Type::a1}, {"a2", Type::a2}, {"b1", Type::b1}};
 /// }
 /// int main(int ac, char* av[]) {
