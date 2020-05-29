d4aee91ee95ab642a97a0a475e10e7d348c54806
Dejan Mircevski <dejan@scylladb.com>
2020-05-28 22:48:39 -0400

    Add multi_column_restriction::is_IN
    Accidentally forgotten in the last commit.
    
    Signed-off-by: Dejan Mircevski <dejan@scylladb.com>

--- cql3/restrictions/multi_column_restriction.hh
+++ cql3/restrictions/multi_column_restriction.hh
@@ -92,6 +92,10 @@ public:
         return wip::to_string(expression);
     }
 
+    bool is_IN() const override {
+        return wip::find_if(expression, operator_type::IN);
+    }
+
 protected:
     virtual void do_merge_with(::shared_ptr<clustering_key_restrictions> other) = 0;
 
