bfaeb2c56ae31a12731a12174d7f9cc11d39a5c3
Dejan Mircevski <dejan@scylladb.com>
2020-05-29 22:42:59 -0400

    Drop partition_key_restrictions::is_EQ()
    Signed-off-by: Dejan Mircevski <dejan@scylladb.com>

--- cql3/restrictions/token_restriction.hh
+++ cql3/restrictions/token_restriction.hh
@@ -148,10 +148,6 @@ public:
     {
         expression = wip::binary_operator{wip::token{}, &operator_type::EQ, _value};
     }
-
-    bool is_EQ() const override {
-        return true;
-    }
 };
 
 class token_restriction::slice final : public token_restriction {
