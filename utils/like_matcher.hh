47f0f578ee326d6a10cdd18634fdf2335476acfd
Dejan Mircevski <dejan@scylladb.com>
2020-08-19 09:29:30 +0200

    utils: Delete class like_matcher
    All users have switched to regex, so this class is now dead code.
    
    Signed-off-by: Dejan Mircevski <dejan@scylladb.com>

--- utils/like_matcher.hh
+++ utils/like_matcher.hh
@@ -23,7 +23,6 @@
 
 #include "bytes.hh"
 #include <boost/regex/icu.hpp>
-#include <memory>
 
 /// Makes a regular expression corresponding to a CQL LIKE pattern.
 ///
@@ -36,26 +35,3 @@
 /// The whole text must match the pattern; thus <code>'abc' LIKE 'a'</code> doesn't match, but
 /// <code>'abc' LIKE 'a%'</code> matches.
 boost::u32regex make_regex_from_like_pattern(bytes_view pattern);
-
-/// Implements <code>text LIKE pattern</code>.
-class like_matcher {
-    class impl;
-    std::unique_ptr<impl> _impl;
-public:
-    /// Compiles \c pattern and stores the result.
-    ///
-    /// \param pattern UTF-8 encoded pattern with wildcards '_' and '%'.
-    explicit like_matcher(bytes_view pattern);
-
-    like_matcher(like_matcher&&) noexcept; // Must be defined in .cc, where class impl is known.
-
-    ~like_matcher();
-
-    /// Runs the compiled pattern on \c text.
-    ///
-    /// \return true iff text matches constructor's pattern.
-    bool operator()(bytes_view text) const;
-
-    /// Resets pattern if different from the current one.
-    void reset(bytes_view pattern);
-};
