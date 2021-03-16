9e2683a0b051112d8bc9715a72d6d1e919a88092
Dejan Mircevski <dejan@scylladb.com>
2021-03-16 12:39:14 -0400

    Fix the multi-column case
    Signed-off-by: Dejan Mircevski <dejan@scylladb.com>

--- test/cql-pytest/test_null.py
+++ test/cql-pytest/test_null.py
@@ -70,6 +70,8 @@ def test_primary_key_in_null(cql, table1):
     cql.execute(f"INSERT INTO {table1} (p,c) VALUES ('{s}', '{s}')")
     with pytest.raises(InvalidRequest, match='null value'):
         cql.execute(cql.prepare(f"SELECT p FROM {table1} WHERE p IN ?"), [None])
+    with pytest.raises(InvalidRequest, match='Invalid null value for IN restriction'):
+        cql.execute(cql.prepare(f"SELECT p FROM {table1} WHERE p='{s}' AND (c) IN ?"), [None])
 
 # Cassandra says "IN predicates on non-primary-key columns (v) is not yet supported".
 def test_regular_column_in_null(scylla_only, cql, table1):
